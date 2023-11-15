5b2ea590b8f67395516d80fd187fa544ea90d4e9
Hala Ahmed <150565915+78HalaAhmed@users.noreply.github.com>
2023-11-15 16:17:09 +0200

    Add files via upload

---
+++ web.php
@@ -0,0 +1,92 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('test', function () {
+//     return " welcome to my first route";
+// });
+
+// Route::get('user/{name}/{age}', function ($name ,$age=0) {
+//     $msg='the username is:'  .$name; 
+//     if($age>0){
+//         $msg .='the age is:'  .$age; 
+//     }
+//     return  $msg;
+// })->whereIn('name',['hala','hind']);
+// // (['name'=>'[a-zA-Z0-9]+','age'=>'[0-9]+'] )
+// // whereAlpha('name')
+// // whereNumber('age')
+// Route::prefix('product')->group(function(){
+
+//     Route::get('/', function (){
+//         return 'home page';
+//    });
+//     Route::get('laptop', function (){
+//          return 'laptop page';
+//     });
+//     Route::get('mobile', function (){
+//         return 'mobile page';
+//    });
+// });
+Route::get("about", function () {
+    return " this is about Page";
+});
+
+Route::get("contact us", function () {
+    return "this is Contact us Page.";
+});
+
+Route::prefix("support")->group(function (){
+    Route::get("/", function () {
+        return "this is Support Page";
+    });
+
+    Route::get("chat", function () {
+        return "this is Chat Page";
+    });
+    
+    Route::get("call", function () {
+        return "this is call Page.";
+    });
+
+    Route::get("ticket", function () {
+        return "this is ticket Page.";
+    });
+});
+
+Route::prefix("training")->group(function () {
+    Route::get("/", function () {
+        return "this is Training Page.";
+    });
+    
+    Route::get("hr", function () {
+        return "this is HR Page.";
+    });
+
+    Route::get("ict", function () {
+        return "this is ict Page.";
+    });
+
+    Route::get("marketing", function () {
+        return "this is Marketing Page.";
+    });
+
+    Route::get("logistics", function () {
+        return "this is Logistics Page.";
+    });
+});
